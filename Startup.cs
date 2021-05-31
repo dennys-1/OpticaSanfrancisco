6eb54b2e0d3677319aa8cf2b9a9de2b0ca845d79
ElvisGC <elarguti@gmail.comm>
2021-05-31 00:58:48 -0500

    credencial2

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
+
+
 namespace OpticaSanfrancisco
 {
     public class Startup
@@ -27,10 +30,14 @@ namespace OpticaSanfrancisco
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
+            
+
+services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(
+Configuration.GetConnectionString("PostgressConnection")));
+services.AddDatabaseDeveloperPageExceptionFilter();
+
+
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
