ce1168f0c0a4e71dbe1d7e90d677f6cc6e5db6d4
70474401 <estrella_yani_13@hotmail.com>
2021-05-31 21:12:35 -0500

    Login

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpticaSanfrancisco.Data;
+
+[assembly: HostingStartup(typeof(OpticaSanfrancisco.Areas.Identity.IdentityHostingStartup))]
+namespace OpticaSanfrancisco.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
