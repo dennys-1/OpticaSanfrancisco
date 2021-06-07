edd85ad5b82bdf028d9bab4bc3d07dd24d37b8b2
ElvisGC <elarguti@gmail.comm>
2021-06-07 04:07:03 -0500

    Nosotros-Mision-Vision

---
+++ Controllers/NosotrosController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpticaSanfrancisco.Models;
+
+namespace OpticaSanfrancisco.Controllers
+{
+    public class NosotrosController : Controller
+    
+    {
+          public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
